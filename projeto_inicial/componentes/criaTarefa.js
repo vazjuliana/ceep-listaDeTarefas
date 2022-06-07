317cf42188babefc24a722d8bfbc6f73a8ec57fc
Juliana Vaz da Silva <vazjuliana1993@gmail.com>
2022-06-07 19:01:46 -0300

    ceep

---
+++ projeto_inicial/componentes/criaTarefa.js
@@ -0,0 +1,7 @@
+import { handleNovoItem } from "./componentes/criaTarefa.js"
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+
+novaTarefa.addEventListener('click', handleNovoItem)
+    
